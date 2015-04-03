92223712e544fad9c1136b4e73e13be779be98eb
keviny22 <kevin_young@intuit.com>
2015-04-03 15:32:12 -0700

    tests_passing

--- lib/classes/http.rb
+++ lib/classes/http.rb
@@ -1,3 +1,4 @@
+require 'httparty'
 module Quickbase
   class HTTP
     include HTTParty
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,8 @@ SimpleCov.start do
   add_filter "/spec/"
 end
 
-Dir[File.expand_path(File.join(File.dirname(__FILE__), "..", "lib", "**", '*.rb'))].each {|f| require f}
+Dir[File.expand_path(File.join(File.dirname(__FILE__), "..", "lib", "**", '*.rb'))].each {|f|
+  f
+  require f
+}
+#require File.dirname(__FILE__) + "/../lib/quickbase.rb"
